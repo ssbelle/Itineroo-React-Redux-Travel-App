de6bc223c28b2cae3e68a1d562b8a05af045f40a
Shawna <shawnalehman@mail.com>
2017-06-20 11:21:44 -0400

    styled

--- react_clientside/src/components/SignupPage.js
+++ react_clientside/src/components/SignupPage.js
@@ -10,6 +10,9 @@ class SignupPage extends React.Component {
     const { userSignupRequest } = this.props;
     return (
       <div className="row">
+      <section className='info-bar'>
+        <div className='direction-bar'></div>
+      </section>
       {/* <div className = "col-md-4 col-md-offset-4" > */}
         <SignupForm userSignupRequest={userSignupRequest}/>
       {/* </div> */}
