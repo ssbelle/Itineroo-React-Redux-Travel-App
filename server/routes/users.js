fecc85ccc77a03f89aac3dc73b2240fdba3e0917
Dong Hu <cdhu@uwaterloo.ca>
2017-06-20 00:13:19 +0000

    Save user into database

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,8 @@
-
 import express from 'express';
 import validateInput from '../shared/validations/signup';
+import bcrypt from 'bcrypt';
+
+import User from '../models/user';
 
 let router = express.Router();
 
@@ -9,7 +11,16 @@ router.post('/', (req, res) => {
   const { errors, isValid } = validateInput(req.body);
 
   if (isValid) {
-    res.json({ success: true });
+    const { username, email, password} = req.body;
+    const password_digest = bcrypt.hashSync(password, 10);
+
+    console.log('USERNAME, EMAIL, PASSWORD', req.body);
+
+    User.forge({
+      username, email, password_digest
+    }, { hasTimestamps: true }).save()
+    .then(user => res.json({success: true}))
+    .catch(err => res.status(500).json({error: err}));
   } else {
     res.status(400).json(errors);
   }
