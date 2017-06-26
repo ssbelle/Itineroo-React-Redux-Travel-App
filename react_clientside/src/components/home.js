cff7b2003e3c270919452850047497111846d6bb
Shawna <shawnalehman@mail.com>
2017-06-26 10:13:12 -0400

    Update with merge conflicts fixed

--- react_clientside/src/components/home.js
+++ react_clientside/src/components/home.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import { AutoRotatingCarousel, Slide } from 'material-auto-rotating-carousel'
-import { green400, green600, blue400, blue600, red400, red600 } from 'material-ui/styles/colors'
 
 // ...
 export default class Home extends React.Component {
 render() {
   return (
-    <div>
+    <section>
+      <section className='info-bar'>
+      
+      </section>
 
-    </div>
+
+      <footer>
+        <div className='footer teal' />
+      </footer>
+    </section>
   );
 }
 }
