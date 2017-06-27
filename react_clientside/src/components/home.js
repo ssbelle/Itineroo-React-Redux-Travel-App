6331f94b38df88bf7487e19cdbb74a03979228c9
Dong Hu <cdhu@uwaterloo.ca>
2017-06-27 15:51:32 -0400

    Implement homepage

--- react_clientside/src/components/home.js
+++ react_clientside/src/components/home.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import ReactDom from 'react-dom';
 
 // ...
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(e) {
+     ReactDom.render(<Popup/>, document.getElementById('signuppage'));
+  }
+
 render() {
   return (
     <header>
@@ -10,7 +22,7 @@ render() {
             <div className="header-content-inner">
                 <h1>Travel</h1>
                 <p>Objectively innovate empowered manufactured products whereas parallel platforms.</p>
-                <Link to='/signup' className="btn btn-primary btn-lg">Signup</Link>
+                <button onClick={this.onClick} className="btn btn-primary btn-lg">Signup</button>
             </div>
         </div>
     </header>
