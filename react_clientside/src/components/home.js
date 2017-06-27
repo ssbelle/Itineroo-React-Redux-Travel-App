7d3ac3422fe5aea774f41b8ec888f80130cf7197
Dong Hu <cdhu@uwaterloo.ca>
2017-06-27 11:47:09 -0400

    Style homepage

--- react_clientside/src/components/home.js
+++ react_clientside/src/components/home.js
@@ -5,16 +5,15 @@ import {Link} from 'react-router-dom';
 export default class Home extends React.Component {
 render() {
   return (
-    <section>
-      <section className='info-bar'>
-      
-      </section>
-
-
-      <footer>
-        <div className='footer teal' />
-      </footer>
-    </section>
+    <header>
+        <div className="header-content">
+            <div className="header-content-inner">
+                <h1>Dramatically Engage</h1>
+                <p>Objectively innovate empowered manufactured products whereas parallel platforms.</p>
+                <a href="#" className="btn btn-primary btn-lg">Engage Now</a>
+            </div>
+        </div>
+    </header>
   );
 }
 }
--- react_clientside/src/components/nav.js
+++ react_clientside/src/components/nav.js
@@ -2,17 +2,17 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import {Link} from 'react-router-dom';
 // import PlacesList from './places_list';
-import { connect } from 'react-redux';
-import { logout } from '../actions/authActions';
+import {connect} from 'react-redux';
+import {logout} from '../actions/authActions';
 
 class Nav extends React.Component {
-  logout (e) {
+  logout(e) {
     e.preventDefault();
     this.props.logout();
   }
 
   render() {
-    const { isAuthenticated } = this.props.auth
+    const {isAuthenticated} = this.props.auth
 
     const userLinks = (
       <ul className="nav navbar-nav navbar-right">
@@ -38,20 +38,72 @@ class Nav extends React.Component {
     );
 
     return (
-      <div>
-      <nav id='navbar' className='navbar teal'>
-        <div className="container-fluid">
+      <nav id="siteNav" className="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div className="container">
+          {/* <!-- Logo and responsive toggle --> */}
           <div className="navbar-header">
-            <Link to="/" className="navbar-brand"><img src='/static/images/travelroo-logo.jpg' /></Link>
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+              <a className="navbar-brand" href="#">
+                <span className="glyphicon glyphicon-fire"></span>
+                LOGO
+              </a>
+              {/* <img src='/static/images/travelroo-logo.jpg' /> */}
           </div>
+          {/* <!-- Navbar links --> */}
+          <div className="collapse navbar-collapse" id="navbar">
+            <ul className="nav navbar-nav navbar-right">
+              <li className="active">
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Products</a>
+              </li>
+              <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services
+                  <span className="caret"></span>
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="about-us">
+                  <li>
+                    <a href="#">Engage</a>
+                  </li>
+                  <li>
+                    <a href="#">Pontificate</a>
+                  </li>
+                  <li>
+                    <a href="#">Synergize</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
 
-          <div className="">
-            { isAuthenticated ? userLinks : guestLinks }
           </div>
+          {/* <!-- /.navbar-collapse --> */}
         </div>
+        {/* <!-- /.container --> */}
       </nav>
-
-      </div>
+    //       {/* <div class="navbar-header">
+    //     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+    //         <span class="sr-only">Toggle navigation</span>
+    //         <span class="icon-bar"></span>
+    //         <span class="icon-bar"></span>
+    //         <span class="icon-bar"></span>
+    //     </button>
+    //     <Link to="/" className="navbar-brand">
+    //       <span class="glyphicon glyphicon-fire"></span>
+    //
+    //     </Link>
+    //     <div className="">
+    //       { isAuthenticated ? userLinks : guestLinks }
+    //     </div>
+    // </div> */}
     );
   }
 }
@@ -62,9 +114,7 @@ Nav.propTypes = {
 };
 
 function mapStateToProps(state) {
-  return {
-    auth: state.auth
-  };
+  return {auth: state.auth};
 }
 
 export default connect(mapStateToProps, {logout})(Nav);
