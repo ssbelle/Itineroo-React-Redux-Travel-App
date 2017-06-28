4d0482df0433985a324ea9bd0e421b43002e3b20
Dong Hu <cdhu@uwaterloo.ca>
2017-06-27 23:41:54 -0400

    Add section to homepage

--- react_clientside/src/components/HomePage/Content.js
+++ react_clientside/src/components/HomePage/Content.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import ReactDom from 'react-dom';
+import FlatButton from 'material-ui/FlatButton';
+import ContentCreate from 'material-ui/svg-icons/content/create';
+import MapsFlight from 'material-ui/svg-icons/maps/flight';
 
 export default class Content extends React.Component {
   constructor(props) {
@@ -19,7 +22,11 @@ export default class Content extends React.Component {
                   <div className="col-sm-6">
                   	<h2 className="section-header">Best in Class</h2>
                   	<p className="lead text-muted">Holisticly predominate extensible testing procedures for reliable supply chains. Dynamically innovate resource-leveling customer service for state of the art customer service.</p>
-                  	<a href="#" className="btn btn-primary btn-lg">Classify It</a>
+                    <Link to="/login">
+                      <FlatButton style={{
+                        color: '#fff'
+                      }} label="Login" labelPosition="before" primary={true} icon={< MapsFlight />}/>
+                    </Link>
                   </div>
 
               </div>
